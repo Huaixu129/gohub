ceaa4984b97ec0312970901eb48e4cd468128de4
huaixu <huaixu129@gmail.com>
2024-11-09 18:30:14 +0800

    图片验证码接口

--- app/http/controllers/api/v1/base_api_controller.go
+++ app/http/controllers/api/v1/base_api_controller.go
@@ -3,4 +3,5 @@ package v1
 
 // BaseAPIController 基础控制器
 type BaseAPIController struct {
+	
 }
--- bootstrap/routes/api.go
+++ bootstrap/routes/api.go
@@ -2,22 +2,26 @@
 package routes
 
 import (
-    "gohub/app/http/controllers/api/v1/auth"
-    
-    "github.com/gin-gonic/gin"
+	"gohub/app/http/controllers/api/v1/auth"
+
+	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
-    // 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-    v1 := r.Group("/v1")
-    {
-        authGroup := v1.Group("/auth")
-        {
-            suc := new(auth.SignupController)
-            // 判断手机是否已注册
-            authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
-        }
-    }
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
+	{
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 发送验证码
+			vcc := new(auth.VerifyCodeController)
+			// 图片验证码，需要加限流
+			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
+		}
+	}
 }
