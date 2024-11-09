a6c525b2eb1ece773324d43a006d3b067fffdcd0
huaixu <huaixu129@gmail.com>
2024-11-09 22:57:13 +0800

    Email 是否已注册接口

--- bootstrap/routes/api.go
+++ bootstrap/routes/api.go
@@ -18,6 +18,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			suc := new(auth.SignupController)
 			// 判断手机是否已注册
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			// 判断 Email 是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
